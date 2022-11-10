77e34d545a916d8bd3ff293b0e45977dc1fc8441
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-10 12:28:58 +0000

    passing test for sum of different numbers

--- review_4/stringSumEvaluator.js
+++ review_4/stringSumEvaluator.js
@@ -1,9 +1,11 @@
 const stringSumEvaluator = (inputString) => {
   splitString = inputString.split(" ");
-  if (splitString.length > 1) {
-    return [inputString, 2];
-  }
-  return [inputString, 1];
+
+  if (splitString.length == 1) return [inputString, 1];
+  console.log(splitString);
+  console.log(parseInt(splitString[2]));
+
+  return [inputString, parseInt(splitString[0]) + parseInt(splitString[2])];
 };
 
 module.exports = stringSumEvaluator;
--- review_4/stringSumEvaluator.test.js
+++ review_4/stringSumEvaluator.test.js
@@ -6,3 +6,9 @@ test("Returns a input as an array of the input and a number", () => {
 test("Returns a calcluation of 1 + 1", () => {
   expect(stringSumEvaluator("1 + 1")).toEqual(["1 + 1", 2]);
 });
+test("Returns a calcluation of 2 + 1", () => {
+  expect(stringSumEvaluator("2 + 1")).toEqual(["2 + 1", 3]);
+});
+test("Returns a calcluation of 1 * 1", () => {
+  expect(stringSumEvaluator("1 * 1")).toEqual(["1 + 1", 1]);
+});
